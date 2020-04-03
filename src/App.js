f4e400f04564b581e24d6a56bc9808a35e16b729
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-03 18:51:55 -0400

    Converted from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopBar from './components/TopBar';
 import ContentBody from './components/ContentBody'
 import AppFooter from './components/footer';
@@ -7,59 +8,104 @@ import 'bulma/css/bulma.css'
 import './css/styles.scss';
 import './css/darkStyles.scss';
 
-class App extends Component {
+const App = () => {
+  const [showCurrentDay, setShowCurrentDay] = useState(true);
+  const [DOTY, setDOTY] = useState(0);
+  const [viewPrevText, setViewPrevText] = useState("View Yesterday's Word");
 
-  state = {
-    showCurrentDay: true,
-    DOTY: 0,
-    viewPrevText: "View Yesterday's Word",
-  }
+  useEffect(() => {
+    getDayOfTheYear();
+  });
 
-  componentDidMount(){
-    this.getDayOfTheYear();
-  }
-
-  getDayOfTheYear = () => {
+  const getDayOfTheYear = () => {
     const now = new Date();
     const start = new Date(now.getFullYear(), 0, 0);
     const diff = now - start;
     const oneDay = 1000 * 60 * 60 * 24;
     const day = Math.floor(diff / oneDay);
-    this.setState({DOTY: day})
+    setDOTY(day);
     return day;
   }
 
-  handleViewPrev = () => {
-    if(this.state.showCurrentDay === true){
-      this.setState({
-        DOTY: this.getDayOfTheYear() - 1,
-        showCurrentDay: false,
-        viewPrevText: "View Today's Word"
-      })
+  const handleViewPrev = () => {
+    if(showCurrentDay === true){
+      setDOTY(DOTY - 1);
+      setShowCurrentDay(false);
+      setViewPrevText("View Today's Word");
     } else {
-      this.setState({
-        DOTY: this.getDayOfTheYear(),
-        showCurrentDay: true,
-        viewPrevText: "View Yesterday's Word"
-      })
+      setDOTY(DOTY + 1);
+      setShowCurrentDay(true);
+      setViewPrevText("View Yesterday's Word");
     }
   }
 
-  render(){
-    return (
-      <>
-        <TopBar
-          buttonText={this.state.viewPrevText}
-          handleViewPrev={this.handleViewPrev}
-        />
-        <ContentBody 
-          DOTY={this.state.DOTY}
-        />
-        <AppFooter />
-      </>
-    );
-  }
+  return (
+    <>
+      <TopBar
+        buttonText={viewPrevText}
+        handleViewPrev={handleViewPrev}
+      />
+      <ContentBody 
+        DOTY={DOTY}
+      />
+      <AppFooter />
+    </>
+  );
 
 }
+
+// class App extends Component {
+
+//   state = {
+//     showCurrentDay: true,
+//     DOTY: 0,
+//     viewPrevText: "View Yesterday's Word",
+//   }
+
+//   componentDidMount(){
+//     this.getDayOfTheYear();
+//   }
+
+//   getDayOfTheYear = () => {
+//     const now = new Date();
+//     const start = new Date(now.getFullYear(), 0, 0);
+//     const diff = now - start;
+//     const oneDay = 1000 * 60 * 60 * 24;
+//     const day = Math.floor(diff / oneDay);
+//     this.setState({DOTY: day})
+//     return day;
+//   }
+
+//   handleViewPrev = () => {
+//     if(this.state.showCurrentDay === true){
+//       this.setState({
+//         DOTY: this.getDayOfTheYear() - 1,
+//         showCurrentDay: false,
+//         viewPrevText: "View Today's Word"
+//       })
+//     } else {
+//       this.setState({
+//         DOTY: this.getDayOfTheYear(),
+//         showCurrentDay: true,
+//         viewPrevText: "View Yesterday's Word"
+//       })
+//     }
+//   }
+
+//   render(){
+//     return (
+//       <>
+//         <TopBar
+//           buttonText={this.state.viewPrevText}
+//           handleViewPrev={this.handleViewPrev}
+//         />
+//         <ContentBody 
+//           DOTY={this.state.DOTY}
+//         />
+//         <AppFooter />
+//       </>
+//     );
+//   }
+// }
 
 export default App;
