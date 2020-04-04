ddefa911abb2e03b6297f442aeaab89649a5c2c6
Thomas Wicks <52025129+ThomasWicksWeb@users.noreply.github.com>
2020-04-04 00:43:46 -0400

    Converted to a functional component

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ const App = () => {
   const [viewPrevText, setViewPrevText] = useState("View Yesterday's Word");
 
   useEffect(() => {
+    console.log("Hello");
     getDayOfTheYear();
-  });
+  }, []);
 
   const getDayOfTheYear = () => {
     const now = new Date();
     const start = new Date(now.getFullYear(), 0, 0);
-    const diff = now - start;
+    const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
     const oneDay = 1000 * 60 * 60 * 24;
     const day = Math.floor(diff / oneDay);
     setDOTY(day);
@@ -53,59 +54,5 @@ const App = () => {
   );
 
 }
-
-// class App extends Component {
-
-//   state = {
-//     showCurrentDay: true,
-//     DOTY: 0,
-//     viewPrevText: "View Yesterday's Word",
-//   }
-
-//   componentDidMount(){
-//     this.getDayOfTheYear();
-//   }
-
-//   getDayOfTheYear = () => {
-//     const now = new Date();
-//     const start = new Date(now.getFullYear(), 0, 0);
-//     const diff = now - start;
-//     const oneDay = 1000 * 60 * 60 * 24;
-//     const day = Math.floor(diff / oneDay);
-//     this.setState({DOTY: day})
-//     return day;
-//   }
-
-//   handleViewPrev = () => {
-//     if(this.state.showCurrentDay === true){
-//       this.setState({
-//         DOTY: this.getDayOfTheYear() - 1,
-//         showCurrentDay: false,
-//         viewPrevText: "View Today's Word"
-//       })
-//     } else {
-//       this.setState({
-//         DOTY: this.getDayOfTheYear(),
-//         showCurrentDay: true,
-//         viewPrevText: "View Yesterday's Word"
-//       })
-//     }
-//   }
-
-//   render(){
-//     return (
-//       <>
-//         <TopBar
-//           buttonText={this.state.viewPrevText}
-//           handleViewPrev={this.handleViewPrev}
-//         />
-//         <ContentBody 
-//           DOTY={this.state.DOTY}
-//         />
-//         <AppFooter />
-//       </>
-//     );
-//   }
-// }
 
 export default App;
